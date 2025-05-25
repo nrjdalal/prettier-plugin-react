8b1e3b4760edc683bbb3cdc7aee483b6ccba90f0
Neeraj Dalal <admin@nrjdalal.com>
2025-05-25 15:59:53 +0530

    chore: tweaks > tsup.config.ts
    package.json
    tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from "tsup"
 
 export default defineConfig({
   clean: true,
-  dts: true,
   entry: {
     index: "src/index.ts",
   },
-  format: ["cjs"],
+  format: ["cjs", "esm"],
   minify: true,
   outDir: "dist",
 })
