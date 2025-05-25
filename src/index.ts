879c4b1e55701d1ed708e2f0daf8096ea689b8b0
Neeraj Dalal <admin@nrjdalal.com>
2025-05-25 15:19:01 +0530

    chore: tweaks > release.yml index.ts test.ts dependencies.yml index.ts config.json package.json.d...
    bun.lock
    .github/workflows/release.yml
    bin/index.ts
    package.json
    bin/commands/test.ts
    .github/workflows/dependencies.yml
    src/index.ts
    .changeset/config.json
    types/package.json.d.ts
    src/utils/generate-id.ts
    .changeset/README.md
    tsup.config.ts
    src/utils/utc-date.ts
    .prettierrc
    .github/changeset-version.js
    .lintstagedrc
    .commitlintrc.ts
    tsconfig.json
    src/utils/index.ts
    LICENSE.md
    .github/FUNDING.yml

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-import { generateId, type GenerateId } from "@/utils/generate-id"
-import { utcDate, type UtcDate } from "@/utils/utc-date"
+import { author, name, version } from "~/package.json"
 
-const date = utcDate()
-const id = generateId(date)
-
-console.log("\x1b[32m%s\x1b[0m", "Generated_ID:", id)
-console.log("\x1b[32m%s\x1b[0m", "UTC_Date:", date)
-
-export { generateId, type GenerateId, utcDate, type UtcDate }
+export const info = {
+  name,
+  version,
+  author,
+}
